dcea7346652f1f4909857fef83e1e245fe760cda
ji-no <mfm.ellegarden@gmail.com>
2023-06-18 11:15:06 +0900

    新しいデータ構造に修正

--- Race.sql
+++ Race.sql
@@ -8,12 +8,13 @@ create table keiba.race(
     stadium text,
     round int,
     raceName text,
-    raceType int, /*0:芝,1;ダート*/
+    raceType int, /*0:芝,1;ダート,3:障害*/
     raceLength int,
     clockwise int, /*0:左回り,1:右回り*/
     raceCondition int, /*0:良,1:鞘重,2:重,3:不良*/
     raceWeather int, /*0:晴,1:曇,2:雨*/
-    grade int, /*0:なし,1:G1,2:G2,3:G3*/
+    grade int, /*0:G1,1:G2,2:G3,3:リステッド,4:オープン,5:3勝クラス,6:2勝クラス,7:1勝クラス,8:新馬,9:未勝利*/,
+    oldLimit int, /*0:4歳以上,1:3歳以上,2:3歳,3:2歳 */
     raceDate date
 );
 create table keiba.horse(
@@ -32,6 +33,7 @@ create table keiba.raceHorse(
     frameNumber int,
     weight int,
     old int,
+    handicap int,
     primary key (
         raceId,horseId,jockeyId
     )
@@ -43,8 +45,14 @@ create table keiba.raceResult(
     cornerRanking text,
     ranking int, 
     lastRapTime int,
+    popular int,
     primary key (
         raceId,frameNumber
     )
 );
+
+create table keiba.trainer(
+    id int auto_increment primary key,
+    name char(255)
+)
 ALTER TABLE keiba.race ADD INDEX raceLenght(raceLength);
