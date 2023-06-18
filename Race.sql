cecf98e61a3843250ad7390378bfbea406a0c2cb
ji-no <mfm.ellegarden@gmail.com>
2023-06-18 16:48:17 +0900

    typo

--- Race.sql
+++ Race.sql
@@ -1,8 +1,8 @@
-drop table raceResult;
+/*drop table raceResult;
 drop table raceHorse;
 drop table horse;
 drop table jockey;
-drop table race;
+drop table race;*/
 create table keiba.race(
     id int auto_increment primary key,
     stadium text,
@@ -13,7 +13,7 @@ create table keiba.race(
     clockwise int, /*0:左回り,1:右回り*/
     raceCondition int, /*0:良,1:鞘重,2:重,3:不良*/
     raceWeather int, /*0:晴,1:曇,2:雨*/
-    grade int, /*0:G1,1:G2,2:G3,3:リステッド,4:オープン,5:3勝クラス,6:2勝クラス,7:1勝クラス,8:新馬,9:未勝利*/,
+    grade int, /*0:G1,1:G2,2:G3,3:リステッド,4:オープン,5:3勝クラス,6:2勝クラス,7:1勝クラス,8:新馬,9:未勝利*/
     oldLimit int, /*0:4歳以上,1:3歳以上,2:3歳,3:2歳 */
     raceDate date
 );
@@ -55,4 +55,4 @@ create table keiba.trainer(
     id int auto_increment primary key,
     name char(255)
 )
-ALTER TABLE keiba.race ADD INDEX raceLenght(raceLength);
+ALTER TABLE keiba.race ADD INDEX raceLength_index(raceLength);
