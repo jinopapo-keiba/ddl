fe214426ec250a7a4c5e46fe44324a0073b2e9f0
ji-no <mfm.ellegarden@gmail.com>
2023-08-20 16:11:46 +0900

    新しくviewを追加

--- Race.sql
+++ Race.sql
@@ -60,3 +60,5 @@ create table trainer(
 
 ALTER TABLE race ADD INDEX raceLength_index(raceLength);
 ALTER TABLE race ADD INDEX raceDate_index(raceDate);
+
+CREATE VIEW horseCount as  select race.id,count(*) from race join raceHorse on race.id=raceHorse.raceId group by race.id;
